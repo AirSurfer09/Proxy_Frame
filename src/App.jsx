64b5fa44aa5466f8a0b02a733247d2df07546ac4
Saurav <138126051+AirSurfer09@users.noreply.github.com>
2025-05-08 15:09:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
             width: "100vw",
             height: "100vh",
           }}
-          src="http://localhost:3000/?sessionid=ae971fb4-2bd2-11f0-ad25-42010a7be01f"
+          src="https://potato-proxy-334fef944b05.herokuapp.com/"
         />
       </div>
     </>
