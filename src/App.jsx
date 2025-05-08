f04aca9b5b9f8b2e27af6df6d10982f94e567e1e
AirSurfer09 <saurav@convai.com>
2025-05-08 12:07:26 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <iframe
+          style={{
+            width: "100vw",
+            height: "100vh",
+          }}
+          src="http://localhost:3000/?sessionid=ae971fb4-2bd2-11f0-ad25-42010a7be01f"
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
